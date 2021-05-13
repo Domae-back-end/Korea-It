373384c2784b0dd507b41d1b0c51d81ba236e2ee
20160 <20160@210.113.69.191>
2021-05-13 23:17:42 +0900

    test

--- movieP/src/main/java/com/controllers/UserController.java
+++ movieP/src/main/java/com/controllers/UserController.java
@@ -25,12 +25,11 @@ public class UserController {
 	public String view(@PathVariable String service, @PathVariable String cate) {
 		//user.main.index 들어가면 메인페이지
 		//user.movie.list 들어가면 영화 리스트 페이지
-		//user.카테고리.(해당jsp이름) 
+		//user.카테고리.(해당jsp이름) d
 		String go = "user/"+service;
 		
 		if(!cate.equals("main")) {
-			if(cate.equals("movie"))
-				go = "user/page/movie/"+service;
+				go = "user/page/"+cate+"/"+service;
 		}
 			
 		return go;
