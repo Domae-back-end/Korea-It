1b6f4ff88543b93d547c6b254f0c9082ac352c6b
inhho <unoestellar@naver.com>
2021-05-18 13:29:02 +0900

    하급배너

--- movieP/Scripts/Script.sql
+++ movieP/Scripts/Script.sql
@@ -79,4 +79,11 @@ CREATE TABLE `noticeboard` (
 	`noticeImg`	varchar(100)
 	
 );
+INSERT INTO bannerimg (bannerindex,imgurl) VALUES
+	 (1,'poster1.jpg'),
+	 (2,'poster2.jpg'),
+	 (3,'poster3.jpg'),
+	 (4,'poster4.jpg'),
+	 (5,'poster5.jpg');
+
 
