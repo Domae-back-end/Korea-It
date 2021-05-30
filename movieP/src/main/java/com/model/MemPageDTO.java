98ecd8cc4a71b52148c6c80d470ffc571efa13c1
82102 <82102@DESKTOP-317QNQV>
2021-05-30 17:14:07 +0900

    my batis

--- movieP/src/main/java/com/model/MemPageDTO.java
+++ movieP/src/main/java/com/model/MemPageDTO.java
@@ -9,18 +9,30 @@ import lombok.Data;
 public class MemPageDTO {
 
 	Integer page = 1, start, startPage, endPage;
-	Integer limit = 6, pageLimit = 4; 
+	Integer limit = 8, pageLimit = 4; 
 	Integer total;
+	String pageKind;
 	
 	public void init(MemberDTO mdto, Map<String, Integer> map) {
+		
+		if(mdto.getPageKind()!=null) {
+			
+			pageKind = mdto.getPageKind().substring(0,1) +"c";
+			
+			if(pageKind.equals("wc"))
+				limit = 5;
+			if(pageKind.equals("lc"))
+				limit = 6;
+		
+		}else
+			pageKind = "qc";
+		
 		
 		if(mdto.getPage()!=null)
 			this.page = mdto.getPage();
 		
 		start = (page - 1) * limit;
-
-		String pageKind = mdto.getPageKind().substring(0,1) +"c";
-		System.out.println(pageKind);
+		
 		Integer ttt = Integer.parseInt(String.valueOf(map.get(pageKind)));
 		
 		this.total = ttt / limit;
