940039fb220324daca184454acb1c29a3c925271
inhho <unoestellar@naver.com>
2021-05-31 16:05:13 +0900

    기초서비스시작 - 예매정보관리기능(/ticketadmin/tclist)

--- movieP/src/main/java/com/controllers/TicketAdminController.java
+++ movieP/src/main/java/com/controllers/TicketAdminController.java
@@ -18,6 +18,7 @@ import com.model.InitData;
 import com.model.Menu;
 import com.model.SalesDTO;
 import com.model.SalesPageDTO;
+import com.model.TicketAdminAction;
 
 @Controller
 @RequestMapping("/admin/ticketadmin/{service}")
@@ -30,15 +31,16 @@ public class TicketAdminController {
 	@ModelAttribute("data")
 	Object data(@PathVariable String service,HttpServletRequest request) {
 		System.out.println("ticketadmin/"+service+"를 실행합니다:");
-		//SalesService sr = pr.getContext().getBean("ticketadmin"+service,SalesService.class);
-		//일단 검색 가능하도록.
+		TicketAdminAction sr = pr.getContext().getBean("ticketadmin"+service,TicketAdminAction.class);
+		//일단 검색 가능하도록.// 
+		//넘버1  Ticketadmintclist
 		
 		Map<String, Object> obj= new HashMap<>();
 		obj.put("request",request);
 		
 	
-		return null;
-		//return sr.execute(obj);//execute결과물 map으로해주면 ,jsp에서 알아서뽑아올수있음.
+	
+		return sr.execute(obj);//execute결과물 map으로해주면 ,jsp에서 알아서뽑아올수있음.
 	}
 
 	@ModelAttribute("bodyurl")
