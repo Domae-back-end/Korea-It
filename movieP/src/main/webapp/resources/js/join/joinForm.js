835440606b717bfd409c595a7f08ab01a9caa78e
82102 <82102@DESKTOP-317QNQV>
2021-06-03 12:13:26 +0900

    왯음?

--- movieP/src/main/webapp/resources/js/join/joinForm.js
+++ movieP/src/main/webapp/resources/js/join/joinForm.js
@@ -1,7 +1,8 @@
 var empCh = /\s/g; 
 var idCh = /^(?=.*[a-zA-Z])(?=.*[0-9_-]).{5,20}$/;
 var pwCh = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
-var nameCh = /^[가-힣]{2,6}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/; 
+/*var nameCh = /^[가-힣]{2,6}|[a-zA-Z]{2,10}\s[a-zA-Z]{2,10}$/; */
+var nameCh = /^[가-힣a-zA-Z0-9]{2,10}$/; 
 var emailCh = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; 
 var pnumCh = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/; 
 
@@ -134,6 +135,22 @@ $(function() {
 		}
 	});
 	
+	$("#pnumM").on("propertychange change keyup paste input", function(){
+		
+		if (pnumCh.test($('#pnumF').val()+$('#pnumM').val()+$('#pnumL').val())) {
+			
+			document.getElementById('userpnum').value = $('#pnumF').val()+'-'+$('#pnumM').val()+'-'+$('#pnumL').val();		
+			
+			$('#pnum_check').text('문자인증을 해주세요.'); 
+			$('#pnum_check').css('color', 'blue');
+			$('#pnumcheck').attr("disabled", false);
+
+		}else{
+			$('#pnum_check').text('휴대폰번호를 확인해주세요.(2~4자 숫자만 사용가능)'); 
+			$('#pnum_check').css('color', 'red');
+			$('#pnumcheck').attr("disabled", true);
+		}
+	});
 	
 	
 	$('#pnumcheck').click(function(){
