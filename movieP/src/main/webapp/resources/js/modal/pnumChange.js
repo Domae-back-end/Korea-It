5fe6835984edc6eab7e3498a91f45102ef18a598
82102 <82102@DESKTOP-317QNQV>
2021-06-06 21:21:35 +0900

    d

--- movieP/src/main/webapp/resources/js/modal/pnumChange.js
+++ movieP/src/main/webapp/resources/js/modal/pnumChange.js
@@ -1,51 +1,62 @@
 /**
  * 
  */
-$(function() {
+var pnumCh = /^01([0|1|6|7|8|9]?)-([0-9]{3,4})-([0-9]{4})$/; 
 
-	$('#checknumButt').click(function() {
-
-
-		var phoneNumber = document.getElementById('newpnum').value;
-
-		alert("인증번호 발송 완료")
-
-		$('#userpnumchk').attr("disabled", true);
+$("#newpnum").on("propertychange change keyup paste input", function() {
 	   
-		
-		$.ajax({
-			type: "POST",
-			url: "/memberpnumCheckSNS",
-			data: { phoneNumber: phoneNumber },
-			success: function(res) {
+		var regexp = /[^0-9-]/gi
+	   $(this).val($(this).val().replace(regexp,''));
+	
+	 
+})
 
-				$('#numfinish').click(function() {
+$('#checknumButt').click(function() {
 
-					if ($.trim(res) == $('#checknum').val()) {
 
-						alert("휴대폰 인증완료")
+	var phoneNumber = document.getElementById('newpnum').value;
 
-						$('#newpnum').attr("disabled", true);
-						$('#checknum').attr("disabled", true);
-						$('#numfinish').attr("disabled", true);
+	alert("인증번호 발송 완료")
 
-					} else {
+	$('#userpnumchk').attr("disabled", false);
+	$('#checknewnum').attr("disabled", false);
+   
+	
+	$.ajax({
+		type: "POST",
+		url: "/memberpnumCheckSNS",
+		data: { phoneNumber: phoneNumber },
+		success: function(res) {
 
-						alert("휴대폰 인증을 다시 해주세요.")
+			$('#checknewnum').click(function() {
 
-						$('#checknum').val("")
-						$('#newpnum').val("")
-						$('#checknum').attr("type", 'hidden');
-						$('#numfinish').attr("type", 'hidden');
-						$('#checknumButt').attr("type", "button");
+				if ($.trim(res) == $('#userpnumchk').val()) {
 
-					}
-				});
+					alert("휴대폰 인증완료")
 
-			}
-		});
+					$('#userpnumchk').val("")
+					$('#checknum').attr("disabled", true);
+					$('#userpnumchk').attr("disabled", true);
+					$('#pnumouter').text($('#newpnum').val());
+					$('#pumnchangGo').val("변경 취소");
+					$('#newpnum').val("");
+					$("#close").trigger("click");
+					
 
+				} else {
+
+					alert("휴대폰 인증을 다시 해주세요.")
+
+					$('#newpnum').val("")
+					$('#userpnumchk').val("")
+					$('#userpnumchk').attr("disabled", true);
+					$('#checknewnum').attr("disabled", true);
+					
+
+				}
+			});
+
+		}
 	});
-
 
 });
