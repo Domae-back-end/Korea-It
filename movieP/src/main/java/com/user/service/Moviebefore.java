288222480e1870ce497f9df74dbd1a96bde9c2c7
20160 <20160@210.113.69.191>
2021-06-07 16:51:55 +0900

    예매율 계산방식 변경

--- movieP/src/main/java/com/user/service/Moviebefore.java
+++ movieP/src/main/java/com/user/service/Moviebefore.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
@@ -18,6 +20,7 @@ import com.model.DbMapper;
 import com.model.MovieAction;
 import com.model.MovieAction6;
 import com.model.MovieInfoDTO;
+import com.model.MovieRateDTO;
 
 
 @Service
@@ -34,45 +37,80 @@ public class Moviebefore implements MovieAction6{
 			MovieInfoDTO aa = it.next();
 			aa.setGpa(mm.calcGPA(aa.getM_index()));
 			aa.setMlike(mm.selectlike(aa.getM_index()));
+			aa.setMovietitle(mm.foundmoviename(aa.getM_index()));
 		}
 		
-		//3일 동안 개봉된 영화의 전체 좌석수를 가져오기(분모)
-		//각 영화의 예매된 좌석수를 가져온다(분자)
-		int cnt =0;
-		Integer[] seatNum = new Integer[3];
-		String[] seatName= {"작은관", "기존관", "3d"};
-		for (String string : seatName) {
-			seatNum[cnt] = mm.pullSeatNum(string);
-			cnt++;
-		}
 		
-		Integer totalSeat = seatNum[0]*100 + seatNum[1]*200 + seatNum[2]*200;
-		//해당 영화의 예매된 좌석 갯수 가져오기
-		//3일동안 해당영화의 예매된 자석 갯수 가져온다.
-		//System.out.println("___________________________________전체 개수: "+totalSeat);
-
-		//System.out.println("전체 개수: "+all);
-		Iterator<MovieInfoDTO> it3 = list.iterator();
-		while(it3.hasNext()) {
-			MovieInfoDTO aa = it3.next();
-			if(totalSeat !=0) {
-				Integer retCnt = mm.allcount(aa.getM_index());
-				Integer ticCnt = mm.detailCount(aa.getM_index());
-				Integer realCnt = ticCnt - retCnt;
-
-				//System.out.println("실제 좌석 차지하는 수 : "+realCnt);
-				double k = Math.round( ((double)realCnt / totalSeat)*100)/100.0;
-				
-				//System.out.println("--------------------------------------영화제목: "+aa.getMovietitle() + "예매율: "+k);
-				aa.setRes_rate(k);
+		
+		//3일 동안 예메된 영화의 전체 좌석수를 가져오기(분모) -- >ticketing 태이블에서 time_index 가져와서 movietime에서 sectorNo가져와서 비교하기
+		//각 영화의 예매된 좌석수를 가져온다(분자) --> time_index 이용하여 movietitle 가져와 저장하고 해당 time_index의 예매된 좌석의 수와 title 일치시키기.
+		List<MovieRateDTO> mrd = mm.pulltime_index();
+		HashMap<String, Integer> rate = new HashMap<String, Integer>();
+		//3일내의 예매된 티켓들의 time_index만 가져옴
+		
+		//분모구하기
+		Integer totalSeat =0;
+		//분모구하기
+		//현재 mrd에는 중복된 time_index가 들어있음.
+		HashSet<MovieRateDTO> hs = new HashSet<MovieRateDTO>(mrd);
+		Iterator<MovieRateDTO> it2 = hs.iterator();
+		while(it2.hasNext()) {
+			MovieRateDTO dd = it2.next();
+			String sec = mm.FoundSector(dd.getTime_index());
+			if(sec.contains("작은관")) {
+				totalSeat += 100;
 			}
 			else {
-				aa.setRes_rate(0.0);
+				totalSeat += 200;
 			}
-			//System.out.println(mm.detailCount(aa.getM_index()));
-			//System.out.println(aa.getMovietitle()+"의 현재 %: "+aa.getRes_rate());	
 		}
 		
+		for(String mapkey : rate.keySet()) {
+			//mapkey == 키
+			int ticketingSeat =0;
+			ticketingSeat += rate.get(mapkey);
+
+			System.out.println("key: "+mapkey+" 현재 예매된 좌석 수: "+ticketingSeat);
+		}
+		
+		//각 영화의 분자구하기
+		Iterator<MovieRateDTO> ii = mrd.iterator();
+		while(ii.hasNext()) {
+			MovieRateDTO s1 = ii.next();
+			s1.setMovietitle(mm.FoundMovietitle(s1.getTime_index()));
+			System.out.println("$#$@$@$@"+s1);
+			if(!rate.containsKey(s1.getMovietitle())) {
+				//만약 해당 영화의 키가 없다면
+				rate.put(s1.getMovietitle(), 1);
+			}
+			else {
+				//해당 영화의 키가 있다면
+				int cnt = rate.get(s1.getMovietitle()) +1;
+				rate.replace(s1.getMovietitle(), cnt);
+			}
+		}
+		
+		System.out.println("%%%%%%%%%%%%%%%%%%%%"+mrd);
+		System.out.println("%%%%%%%%%%%%%%%%%%%%"+rate);
+
+		
+		Iterator<MovieInfoDTO> it3 = list.iterator();
+		while(it3.hasNext()) {
+			MovieInfoDTO dd = it3.next();
+			if(rate.get(dd.getMovietitle()) != null) {
+				System.out.println("&&&&&&&&&&&&&&&&&&&&&&"+rate.get(dd.getMovietitle()) / totalSeat);
+				 double k = Math.round( ((double)rate.get(dd.getMovietitle()) / totalSeat)*100)/100.0;
+				dd.setRes_rate(k);
+			}else {
+				dd.setRes_rate(0.0);
+			}
+			System.out.println("제목: "+dd.getMovietitle() + "예매율: "+dd.getRes_rate());
+		}
+
+
+		
+		//계산된 평점은 해당 movieinfo의 res_rate에 저장.
+		//MovieInfoDTO의 res_Rate로 따진다.
 		Collections.sort(list, new Comparator<MovieInfoDTO>() {
 			@Override
 			public int compare(MovieInfoDTO o1, MovieInfoDTO o2) {
