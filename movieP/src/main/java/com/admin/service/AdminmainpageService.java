14cf081255f6843dc303d9851beddb12ab9ce513
inhho <unoestellar@naver.com>
2021-06-09 10:07:20 +0900

    admin메인페이지 밑작업시작

--- movieP/src/main/java/com/admin/service/AdminmainpageService.java
+++ movieP/src/main/java/com/admin/service/AdminmainpageService.java
@@ -1,8 +1,53 @@
 package com.admin.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public class AdminmainpageService {
+import com.model.DbMapper;
 
+
+
+
+
+
+@Service
+public class AdminmainpageService implements Admincoreservice{
+
+@Resource
+DbMapper db;
+
+@Override
+public Object execute(Map<String, Object> obj) {
+	
+	HashMap<String, Object> resultmap = new HashMap<>();
+	//1 고객문의 db  
+	
+	
+	
+	
+	///2 매출
+	
+	
+	
+	// 3 FAQ
+	
+	
+	
+	//4 최근공지
+	
+	
+	
+	
+	// 맵하나 내밷고 .
+	
+	return resultmap;
+}
+
+	
+	
+	
 }
